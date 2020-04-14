3d4ecd0846dcd7eff76d8c89725cee26714eac86
Zhan.Shi <g.shizhan.g@gmail.com>
2020-04-15 00:09:40 +0800

    scoring

--- cachelab-extended/test-csim.sh
+++ cachelab-extended/test-csim.sh
@@ -1,15 +1,15 @@
-printf "         Your simulator\n"
-printf "(s,E,b)  %-10s %-10s %-10s\n" Hits Misses Evicts
-while read s e b t; do
-    printf "(%s,%s,%s)  %-10s %-10s %-10s  %s\n" $s $e $b \
-        `./csim -s $s -E $e -b $b -t $t | sed 's/[^0-9 ]*//g'` \
-        $t
+printf "              Your simulator\n"
+printf "     (s,E,b)  %-10s %-10s %-10s\n" Hits Misses Evicts
+while read s e b t r; do
+    result=`./csim -s $s -E $e -b $b -t $t | sed 's/[^0-9 ]*//g'`
+    [ "$result" == "$r" ] && printf "Pass " || printf "     "
+    printf "(%s,%s,%s)  %-10s %-10s %-10s  %s\n" $s $e $b $result $t
 done <<< \
-"""1 1 1 traces/yi2.trace
-4 2 4 traces/yi.trace
-2 1 4 traces/dave.trace
-2 1 3 traces/trans.trace
-2 2 3 traces/trans.trace
-2 4 3 traces/trans.trace
-5 1 5 traces/trans.trace
-5 1 5 traces/long.trace"""
+"""1 1 1 traces/yi2.trace 9 8 6
+4 2 4 traces/yi.trace 4 5 2
+2 1 4 traces/dave.trace 2 3 1
+2 1 3 traces/trans.trace 167 71 67
+2 2 3 traces/trans.trace 201 37 29
+2 4 3 traces/trans.trace 212 26 10
+5 1 5 traces/trans.trace 231 7 0
+5 1 5 traces/long.trace 265189 21775 21743"""
