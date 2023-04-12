0e6360278c175882c74371a897e6b8856870354b
Zhan.Shi <g.shizhan.g@gmail.com>
2023-04-12 15:41:48 +0800

    raid-sim options, for running simulation in a batch.

--- raid-lab/raid-sim.py
+++ raid-lab/raid-sim.py
@@ -4,28 +4,67 @@
 # We can repeat this simulation many times and calculate the average time until failure to estimate the MTTF.
 
 import random
+from argparse import *
 
-num_disks = 5
-annual_failure_prob = 0.05
-
-def simulate_disk_failure():
+def simulate_disk_failure(prob):
     """Simulate the failure of a single disk."""
-    return random.random() < annual_failure_prob
+    while True:
+        yield random.random() < prob
 
-def simulate_array_failure():
+def simulate_array_failure(disks, redundancy):
     """Simulate the failure of the entire array."""
-    if sum([simulate_disk_failure() for i in range(num_disks)]) >= 1:
+    if sum([next(disks[i]) for i in range(len(disks))]) > redundancy:
         return True
     return False
 
-def simulate_mttf(num_simulations):
+def simulate_mttf(num_simulations, num_disks, prob, redundancy, mttr):
     """Simulate the MTTF of the array using the given number of simulations."""
+    disks = []
+    for i in range(num_disks):
+        disks.append(simulate_disk_failure(prob))
+
     total_failure = 0
     for i in range(num_simulations):
-        if simulate_array_failure():
+        if simulate_array_failure(disks, redundancy):
             total_failure += 1
+
     return num_simulations / total_failure
 
-# Example usage:
-mttf = simulate_mttf(80000)
-print("Estimated MTTF:", mttf)
+def main():
+    parser = ArgumentParser(description='RAID MTTF Simulator')
+    parser.add_argument('-n', '--numdisks',
+                        type=int,
+                        default=5,
+                        help='Number of disks in the RAID array. Default=5')
+
+    parser.add_argument('-f', '--failureprob',
+                        type=float,
+                        default=0.05,
+                        help='Annual failure probability. Default=0.05')
+
+    parser.add_argument('-e', '--redundancy',
+                        type=int,
+                        default=0,
+                        help='Number of redundancy disks. Default=0 (no redundancy)')
+
+    parser.add_argument('-r', '--mttr',
+                        type=int,
+                        default=0,
+                        help='Mean time to recover. Default=0 (immediately)')
+
+    parser.add_argument('-l', '--elapsing',
+                        type=int,
+                        default=80000,
+                        help='Simulation time length. Default=80000')
+
+    args = parser.parse_args()
+    num_disks = args.numdisks
+    failure_prob = args.failureprob
+    redundancy = args.redundancy
+    mttr = args.mttr
+    elapsing = args.elapsing
+
+    mttf = simulate_mttf(elapsing, num_disks, failure_prob, redundancy, mttr)
+    print("Estimated MTTF:", mttf)
+
+main()
