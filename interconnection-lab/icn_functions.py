1db95c7011a26a6afc1da6402213a3bd822a9fac
Zhan.Shi <g.shizhan.g@gmail.com>
2023-04-28 09:59:46 +0800

    all ICN functions

--- interconnection-lab/icn_functions.py
+++ interconnection-lab/icn_functions.py
@@ -15,11 +15,11 @@ def shuffle_sub(x, k):
 
 def shuffle_sup(x, k, n):
     """均匀洗牌函数第k个超函数"""
-    mask = (1 << (k - 1)) - 1
-    mask_sup = (1 << n) - 1
+    mask = (1 << (n - k)) - 1
+    mask_n_bits = (1 << n) - 1
     low_bits = x & mask
     high_bits = x - low_bits
-    return low_bits | ((high_bits << 1) & mask_sup) | ((high_bits >> (k - 1)) & (mask_sup - mask))
+    return low_bits | ((high_bits << 1) & mask_n_bits) | ((high_bits >> (k - 1)) & (mask_n_bits - mask))
 
 def _swap_bits(x, i, j):
     # Get the bit values of position i and j
@@ -47,23 +47,18 @@ def butterfly_sup(x, k, n):
     """蝶式函数第k个超函数"""
     return _swap_bits(x, n-k-1, n-1)
 
-# Under construction, to be verified ...
-
-def bit_reverse(x, n):
+def bit_reverse(x, k):
     """反位序函数"""
+    mask = (1 << k) - 1
+    low_bits = x & mask
+    high_bits = x - low_bits
     result = 0
-    for i in range(n):
+    for i in range(k):
         result <<= 1
         result |= (x & 1)
         x >>= 1
-    return result
+    return result | high_bits
 
-def cyclic_shift(x, n):
-    """移数函数：循环左移 n 位"""
-    mask = (1 << n) - 1
-    high_bits = (x >> (32 - n)) & mask
-    return ((x << n) & 0xffffffff) | high_bits
-
-def pm2i(x, n):
-    """PM2I函数：将 x 映射到 [0, n-1]"""
-    return (x * 31) % n
+def pm2i(x, k, n):
+    """PM2I函数"""
+    return (x + (1 << k)) & ((1 << n) - 1)
